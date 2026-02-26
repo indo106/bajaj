488b4ceddb05c25948e56b578381c1673ba14e34
indo106 <meshoonlineshopping77@gmail.com>
2026-02-26 15:20:16 +0000

    updated frontend and backend code

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://lxiitvjhbpkatllnzpys.supabase.co'
-const supabaseAnonKey = 'sb_publishable_etbaiHYV6rIWObwFgH2iCg_hNbTQvVQ'
+// Vite mein variables ko access karne ka sahi tarika
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
